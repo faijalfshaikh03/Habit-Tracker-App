254560506160ea8c0541f114da9ce1325a5973c9
faijalfshaikh <faijalfshaikh@gmail.com>
2026-03-03 12:06:21 +0530

    Added new dependencies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,16 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{title:"Home"}}></Stack.Screen>
-    <Stack.Screen name="login" options={{title:"Login"}}></Stack.Screen>
-  </Stack>
+  return <Tabs screenOptions={{tabBarActiveTintColor:"coral"}}>
+    <Tabs.Screen name="index" options={{title:"Home", 
+    tabBarIcon: ({color}) => (
+      <FontAwesome5 name="home" size={24} color={color} />
+    )}}></Tabs.Screen>
+    <Tabs.Screen name="login" options={{title:"Login",
+     tabBarIcon: ({color}) => (
+      <Entypo name="login" size={24} color={color} />
+    )}}></Tabs.Screen>
+  </Tabs>
 }
