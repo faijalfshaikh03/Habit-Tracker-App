f28c4a0a393196ae66ff22b3982ffb80e94ef7b9
faijalfshaikh <faijalfshaikh@gmail.com>
2026-03-03 11:48:23 +0530

    Adding inital files

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+    return <View>
+
+        <Text>Hello Faisu, welcome to android development</Text>
+    </View>
+}
